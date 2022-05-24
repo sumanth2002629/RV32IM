6654feeea212ba525f4ff477510b267592f489b6
BTV Sumanth <76842817+sumanth2002629@users.noreply.github.com>
2022-05-24 18:28:49 +0530

    Add files via upload

---
+++ RV32IM/test_programs/program1/program3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main()
+{
+  	int a, b, c, d, e;
+ 
+ 	a=5; 
+    b=5;
+    c=a+b;//10
+    d=b+c;//15
+    e=c+d;//25
+
+ 
+    
+    printf("e=%d\n", e);
+  
+   	return 0;
+}
---
+++ RV32IM/test_programs/program2/programs.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ 
+int main()
+{
+  int number, cube;
+ 
+  number = 3;
+  
+  cube = number * number * number;
+  
+  printf("\n Cube of a given number %d is  =  %d", number, cube);
+ 
+  return 0;
+}
